aff31f9b37ed0882bdee7bbdfa2fb6b54c05d060
dong1hyun <limd1238@naver.com>
2024-02-27 13:43:45 +0900

    mysql 데이터베이스 연결 성공
    DB이름은 test이기 때문에 일단 new_schema이고 나중에 바꿀 예

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const { sequelize } = require('./models')
+
+console.log("dd")
 
 app.set('port', 3000);
+nunjucks.configure('views', {
+    express: app,
+    watch: true,
+});
+
+sequelize.sync({force:false})
+.then(() => {
+    console.log("DB연결 성공");
+})
+.catch((err) => {
+    console.log("ddd")
+    console.error(err);
+})
+
+app.use(morgan('dev'));
 
 app.listen(8080, function() {
     console.log("listening on 8080");
