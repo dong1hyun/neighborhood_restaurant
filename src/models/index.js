211194fb55d039029f1c61d627fece821251ef92
dong1hyun <limd1238@naver.com>
2024-03-07 11:31:55 +0900

    define과 file을 읽어오는 방식으로 db구현

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,39 @@
 const Sequelize = require('sequelize');
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
-const User = require('./user');
-const Review = require('./review');
-const Restaurant = require('./restaurant');
+const fs = require('fs');
+const path = require('path');
+const basename = path.basename(__filename);
 const db = {};
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+let sequelize;
+
+//config/config.js 파일에 있는 정보를 가져와 sequelize 객체를 생성한다.
+if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+// 우리가 작성한 Table파일을 찾아온다.
+fs
+    .readdirSync(__dirname)
+    .filter(file => {
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
+    });
+
+//DB에 모델이름을 연결한다.
+Object.keys(db).forEach(modelName => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
+});
 
 db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
-db.User = User;
-db.Review = Review;
-db.Restaurant = Restaurant;
-
-User.init(sequelize);
-Review.init(sequelize);
-Restaurant.init(sequelize);
-
-User.associate(db);
-Review.associate(db);
-Restaurant.associate(db);
-
-// module.exports = db;
+module.exports = db;
