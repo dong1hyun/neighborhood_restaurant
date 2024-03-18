368a5370cc8420835890ecd50376173cdd8b7de4
최성준 <choi000803@naver.com>
2024-03-18 15:28:21 +0900

    리액트 및 Express 연동

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const { sequelize } = require('./models');
 const passport = require('passport');
 const passportConfig = require('./passport');
 
+const bodyParser = require('body-parser');
+const { useNavigate } = require("react-router");
+
+
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/user');
 const commentRouter = require('./routes/comment');
@@ -56,6 +60,15 @@ app.use('/user', userRouter);
 app.use('/comment', commentRouter);
 app.use('/', indexRouter);
 
+// 빌드랑 연결 라우터
+app.use( express.static( path.join(__dirname, '../build') ) );
+app.get('/', function(req, res){
+    res.sendFile( path.join(__dirname, '../build/index.html') )
+});
+app.get('*', function(req, res){
+    res.sendFile( path.join(__dirname, '../build/index.html') )
+});
+
 app.use((req, res) =>
     res.render('index', {
         title: require('./package.json').name,
@@ -71,3 +84,20 @@ app.use((err, req, res, next) => {
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
+
+
+// 식당데이터저장
+// app.post('/create/restaurant', function(req, res) {
+//     // console.log(req.body);
+//     const restaurantList = req.body;
+//     restaurantList.forEach(place => {
+//         console.log(place.place_name.length + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@)")
+//         Restaurant.findOrCreate({
+//             where: { id: place.id, name: place.place_name },
+//             default: {
+//                 id: place.id,
+//                 name: place.place_name
+//             }
+//         })
+//     }) 
+// })
--- src/server.js
+++ src/server.js
@@ -1,59 +1,59 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const morgan = require('morgan');
-const nunjucks = require('nunjucks');
-const { sequelize } = require('./models')
-const bodyParser = require('body-parser');
-const {User, Review, Restaurant} = require("./models");
-const { useNavigate } = require("react-router");
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const morgan = require('morgan');
+// const nunjucks = require('nunjucks');
+// const { sequelize } = require('./models')
+// const bodyParser = require('body-parser');
+// const {User, Review, Restaurant} = require("./models");
+// const { useNavigate } = require("react-router");
 
-app.set('port', 3000);
-nunjucks.configure('views', {
-    express: app,
-    watch: true,
-});
+// app.set('port', 8080);
+// nunjucks.configure('views', {
+//     express: app,
+//     watch: true,
+// });
 
-sequelize.sync({force:false})
-.then(() => {
-    console.log("DB연결 성공");
-})
-.catch((err) => {
-    console.error(err);
-})
+// sequelize.sync({force:false})
+// .then(() => {
+//     console.log("DB연결 성공");
+// })
+// .catch((err) => {
+//     console.error(err);
+// })
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(morgan('dev'));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.listen(8080, function() {
-    console.log("listening on 8080");
-})
+// app.listen(8080, function() {
+//     console.log("listening on 8080");
+// })
 
-console.log(Restaurant)
-app.post('/create/restaurant', function(req, res) {
-    // console.log(req.body);
-    const restaurantList = req.body;
-    restaurantList.forEach(place => {
-        console.log(place.place_name.length + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@)")
-        Restaurant.findOrCreate({
-            where: { id: place.id, name: place.place_name },
-            default: {
-                id: place.id,
-                name: place.place_name
-            }
-        })
-    }) 
-})
+// console.log(Restaurant)
+// app.post('/create/restaurant', function(req, res) {
+//     // console.log(req.body);
+//     const restaurantList = req.body;
+//     restaurantList.forEach(place => {
+//         console.log(place.place_name.length + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@)")
+//         Restaurant.findOrCreate({
+//             where: { id: place.id, name: place.place_name },
+//             default: {
+//                 id: place.id,
+//                 name: place.place_name
+//             }
+//         })
+//     }) 
+// })
 
 
+// // 빌드랑 연결 라우터
+// app.use( express.static( path.join(__dirname, '../build') ) );
 
-app.use( express.static( path.join(__dirname, '../build') ) );
+// app.get('/', function(req, res){
+//     res.sendFile( path.join(__dirname, '../build/index.html') )
+// });
 
-app.get('/', function(req, res){
-    res.sendFile( path.join(__dirname, '../build/index.html') )
-});
-
-app.get('*', function(req, res){
-    res.sendFile( path.join(__dirname, '../build/index.html') )
-});
+// app.get('*', function(req, res){
+//     res.sendFile( path.join(__dirname, '../build/index.html') )
+// });
