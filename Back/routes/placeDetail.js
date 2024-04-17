10b4d626baefb917a5c905e63a64338714490c25
최성준 <choi000803@naver.com>
2024-04-17 15:53:04 +0900

    feat. restaurant and placeDetail change
    수정 완료 및 수정중

--- Back/routes/placeDetail.js
+++ Back/routes/placeDetail.js
@@ -19,3 +19,27 @@ router.get('/:id', function(req, res) {
 });
 
 module.exports = router;
+
+
+// 수정) 음식점 상세 정보 안에서 작성된 해당 음식점 리뷰 조회하기. 클라이언트 Place?에서 요청하는 걸로 예상중입니다.
+
+// router.get('/:id', async function(req, res) {
+//     try {
+//         const restaurant = await Restaurant.findOne({
+//             where: { restaurantID: req.params.id }
+//         });
+
+//         if (!restaurant) {
+//             return res.status(404).json({ error: "Restaurant not found" });
+//         }
+
+//         const reviews = await Review.findAll({
+//             where: { restaurantID: req.params.id }
+//         });
+
+//         res.json({ restaurant, reviews });
+//     } catch (error) {
+//         console.error("Error fetching restaurant:", error);
+//         res.status(500).json({ error: "Internal Server Error" });
+//     }
+// });
--- Back/routes/restaurant.js
+++ Back/routes/restaurant.js
@@ -3,47 +3,45 @@ const router = express.Router();
 const { Restaurant } = require('../models');
 const axios = require('axios'); // axios 모듈 가져오기
 
+
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+
 
 //client/function/search P.79 -> Back/server/restaurant 
 router.post('/', async (req, res) => {
     try {
         const restaurantList = req.body;
         for (const place of restaurantList) {
-            // 데이터베이스에 이미 해당 식당이 존재하는지 확인
             const existingRestaurant = await Restaurant.findOne({ where: { restaurantID: place.id } });
             if (existingRestaurant) {
-                console.log(`식당 ID ${place.id}는 이미 데이터베이스에 존재합니다.`);
-                continue; // 이미 존재하면 다음 식당으로 넘어감
+                // 만약 레스토랑이 이미 존재하면 추가하지 않고 넘어갑니다.
+                continue;
             }
 
-            // 존재하지 않는 경우에만 데이터베이스에 저장
+            // 존재하지 않는 경우에만 데이터베이스에 저장\
             const crawlingData = await axios.get(`https://place.map.kakao.com/m/main/v/${place.id}`)
             let img_url = crawlingData.data?.basicInfo?.mainphotourl;
-            let timeList = JSON.stringify(crawlingData.data.basicInfo?.openHour?.periodList[0]?.timeList);
-            img_url = img_url ? img_url : "none";
-            timeList = timeList ? timeList : "none";
-
-            const [restaurant, created] = await Restaurant.findOrCreate({
-                where: { restaurantID: place.id },
-                defaults: {
-                    restaurantName: place.place_name,
-                    address: place.address_name,
-                    Category: place.category_name.substr(6),
-                    restaurantNumber: place.phone,
-                    img: img_url,
-                    timeList: timeList,
-                    x: place.x,
-                    y: place.y
-                }
-            });
-
-            if (created) {
-                console.log(`식당 ID ${place.id}가 성공적으로 추가되었습니다.`);
-            } else {
-                console.log(`식당 ID ${place.id}가 이미 데이터베이스에 존재합니다.`);
+            let timeList;
+            if(crawlingData.data.basicInfo?.openHour?.periodList){
+                timeList = JSON.stringify(crawlingData.data.basicInfo?.openHour?.periodList[0]?.timeList);
             }
+        
+                img_url = img_url ? img_url : "none";
+                timeList = timeList ? timeList : "none";
+            
+
+            await Restaurant.create({
+                restaurantID: place.id,
+                restaurantName: place.place_name,
+                address: place.address_name,
+                Category: place.category_name.substr(6),
+                restaurantNumber: place.phone,
+                img: img_url,
+                timeList: timeList,
+                x: place.x,
+                y: place.y
+            });
         }
         res.status(200).send('레스토랑이 성공적으로 추가되었습니다.');
     } catch (error) {
@@ -51,5 +49,6 @@ router.post('/', async (req, res) => {
         res.status(500).send('내부 서버 오류가 발생했습니다.');
     }
 });
+
 
 module.exports = router;
