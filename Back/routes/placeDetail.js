adcd0063411d2b41b19a942e1dd974f7044ba22d
최성준 <choi000803@naver.com>
2024-05-12 23:49:24 +0900

    feat. 추천음식점 수정중

--- Back/routes/placeDetail.js
+++ Back/routes/placeDetail.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { Restaurant } = require('../models');
+const { Op } = require('sequelize');
+
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
+
 
 router.get('/:id', function(req, res) {
     Restaurant.findOne({
@@ -22,29 +25,50 @@ router.get('/:id', function(req, res) {
         res.status(500).json({ error: "Internal Server Error" });
     });
 });
+ 
 
-// 추천음식점 조회
-// router.get('/recommended', async (_req, res) => {
+// 주소 말고, 거리순으로 고려해보자.
+// // 유사한 음식점 가져오는 라우트
+// router.get('/:id/similar', async function(req, res) {
 //     try {
-//         // 음식점 테이블에서 랜덤하게 5개의 레코드를 가져옵니다.
-//         const restaurants = await Restaurant.findAll({
-//             order: sequelize.random(), // Sequelize에서 지원하는 랜덤 정렬
-//             limit: 5 // 5개의 레코드만 선택
+//         const restaurantId = req.params.id;
+
+//         // 주어진 restaurantId의 주소를 가져오기
+//         const { restaurantAddress } = await Restaurant.findOne({
+//             where: { restaurantId: restaurantId }
+//         });
+
+//         // 숫자를 제외한 문자열로 주소를 변환합니다.
+//         const cleanedAddress = restaurantAddress.replace(/\d+/g, '').trim().replace(/-$/, '');
+
+
+//         console.log('요기',cleanedAddress);
+
+//         // 동일한 주소를 가진 다른 음식점을 가져옵니다.
+//         const similarRestaurants = await Restaurant.findAll({
+//             where: { 
+//                 restaurantAddress: cleanedAddress, // cleanedAddress 값과 동일한 주소를 가진 음식점을 선택합니다.
+//                 restaurantId: { [Op.ne]: restaurantId } // 주어진 음식점 아이디는 제외
+//             },
+//             limit: 5 // 5개의 음식점만 가져오기
 //         });
         
-//         // 각 음식점의 이미지 정보만 추출하여 배열에 담습니다.
-//         const restaurantData = restaurants.map(restaurant => ({
-//             restaurantId: restaurant.dataValues.restaurantId,
-//             restaurantName: restaurant.dataValues.restaurantName,
-//             img: restaurant.dataValues.img
+//         console.log('요기',similarRestaurants);
+
+
+//         const formattedRestaurants = similarRestaurants.map(restaurant => ({
+//             restaurantId: restaurant.restaurantId,
+//             restaurantName: restaurant.restaurantName,
+//             img: restaurant.img
 //         }));
-        
-//         res.json({ restaurantData }); // 클라이언트에 이미지 배열을 JSON 형태로 응답합니다.
+
+//         res.json(formattedRestaurants);
 //     } catch (error) {
-//         console.error('Error fetching random restaurants:', error);
-//         res.status(500).json({ error: 'An error occurred while fetching random restaurants' }); // 에러 발생 시 500 상태코드와 에러 메시지를 응답합니다.
+//         console.error("유사한 음식점을 가져오는 중 에러 발생:", error);
+//         res.status(500).json({ error: "내부 서버 오류" });
 //     }
 // });
+
 
 
 module.exports = router;
