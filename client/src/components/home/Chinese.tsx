9d3372db5b0067f92cfffd46104c087da6cfd331
dong1hyun <limd1238@naver.com>
2024-05-23 22:01:21 +0900

    styled.components 폴더 위치 변경

--- client/src/components/home/Chinese.tsx
+++ client/src/components/home/Chinese.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Container, NextBtn, PlaceBox, PlaceImg, PlaceTitle, PrevBtn, Rating, Slider } from "../styled-components/homeStyle";
+import { Container, NextBtn, PlaceBox, PlaceImg, PlaceTitle, PrevBtn, Rating, Slider } from "../../styled-components/homeStyle";
 import { SliderVar, restaurantForm } from "../../lib/homeLib"
 import { AnimatePresence } from "framer-motion";
 
