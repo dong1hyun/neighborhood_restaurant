3f0f8f92a2b35e25ea06a417c4379d55cd7c6885
최성준 <choi000803@naver.com>
2024-10-24 02:13:20 +0900

    최신화

--- Back/server.js
+++ Back/server.js
@@ -99,8 +99,8 @@ const options = {
 
 
 // HTTPS 의존성으로 certificate와 private key로 새로운 서버를 시작
-https.createServer(options, app).listen(443, '192.210.220.61', () => {
-    console.log(`HTTPS server started on https://192.210.220.61`);
+https.createServer(options, app).listen(443, '192.168.0.40', () => {
+    console.log(`HTTPS server started on https://192.168.0.40`);
 });
 
 
