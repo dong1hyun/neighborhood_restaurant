a8705f6fd397334e1faf926d837df64bd40a5775
최성준 <choi000803@naver.com>
2024-11-19 23:57:05 +0900

    Update server.js

--- Back/server.js
+++ Back/server.js
@@ -92,17 +92,18 @@ app.use('/like', likeRouter);
 // });
 
 // Build 폴더를 정적 파일로 서빙
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'Back', 'build')));
 
 // 루트 경로로 들어왔을 때 React의 index.html 파일 서빙
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Back', 'build', 'index.html'));
 });
 
 // React 라우터 처리를 위한 fallback route
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Back', 'build', 'index.html'));
 });
+
 
 
 // const options = {
